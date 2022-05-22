886b85c51a535eb4b0ec7616adcf0dcafc8f9693
Monica calle <monicacalle369@gmail.com>
2022-05-22 16:31:08 -0400

    creating a robofriends page using react js

---
+++ src/containers/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import CardList from "../components/CardList";
+import { robots } from "../robots";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      Searchfield: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => {
+        this.setState({ robots: users });
+      });
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ Searchfield: event.target.value });
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter((robots) => {
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.Searchfield.toLowerCase());
+    });
+    if (robots.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+    }
+  }
+}
+
+export default App;
